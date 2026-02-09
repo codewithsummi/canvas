8050cedbee0050fe600630afd820dec6bb6083e1
“sumit” <sumit.ducat@gmail.com>
2026-02-09 20:55:28 +0530

    third

--- js/app.js
+++ js/app.js
@@ -13,3 +13,48 @@ const ctx=canvas.getContext("2d");
 //setting canvas size 
 canvas.height=420;
 canvas.width=800;
+
+//set state variables (app memory)
+let isDrawing=false; //currently drawing anything with freehand
+let currentTool="pen"; // "pen" or "eraser"
+let isDrawingSquare=false; //if true , then square /rectangle mode is enable 
+
+//setting default brush styles 
+ctx.strokeStyle=colorPicker.value; //line color
+ctx.lineWidth=brushSizeSelector.value;//brush thickness
+ctx.lineCap="round" // makes line ends rounded (smoother drawing)
+
+//variable for rectangle start points
+let startX=0;
+let startY=0;
+
+//Drawing function 
+
+function startDraw(e)//when mouse is pressed down
+{
+   if(isDrawingSquare){
+       startX=e.offsetX;
+       startY=e.offsetY;
+       return;
+   }
+   isDrawing=true;
+   ctx.beginPath();//start a new path 
+   ctx.moveTo(e.offsetX,e.offsetY)
+}
+
+function draw(e)//when mouse moves
+{
+
+}
+function stopDrawing(e)//when mouse is released
+{
+
+}
+//tool buttons (event listeners)
+eraserTool.addEventListener("click",()=>{
+    isDrawingSquare=false;
+    penTool.classList.remove("active")
+    eraserTool.classList.add("active")
+    drawSquareBtn.classList.remove("active")
+    currentTool="eraser";
+})
